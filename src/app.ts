8c684f618f0d26036a21f8e64ec89edb2f4e2fad
dah <cdh010126r@gmail.com>
2024-07-20 16:32:28 +0900

    fix: app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
 // swagger setting
 const swaggerUi = require("swagger-ui-express");
@@ -18,3 +19,14 @@ const eventRouter = require("./routes/EventRoutes");
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/events", eventRouter);
+// 기본 라우트
+app.get("/", (req, res) => {
+  res.send("Welcome to the Express server!");
+});
+
+// 서버 시작
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+export default app;

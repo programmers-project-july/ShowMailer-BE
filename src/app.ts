af95adffd758fbfa5cf6edc2940df0813fa5155f
jieunlee99 <lll3346@naver.com>
2024-07-20 16:07:26 +0900

    app.ts 수정

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+// swagger setting
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger/swagger-output.json");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, function () {
+  console.log("Server open.");
+});
+
+const eventRouter = require("./routes/EventRoutes");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/events", eventRouter);

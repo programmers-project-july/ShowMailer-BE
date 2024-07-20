93070c979d8383aa612a7b6fa001e0a83c3d1538
dah <cdh010126r@gmail.com>
2024-07-20 17:18:27 +0900

    fix: dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,18 @@
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-
 // swagger setting
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("../swagger/swagger-output.json");
 
-const dotenv = require("dotenv");
-dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Swagger UI 설정
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(express.json());
 
-app.listen(process.env.PORT, function () {
-  console.log("Server open.");
-});
-
 const eventRouter = require("./routes/EventRoutes");
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/events", eventRouter);
 // 기본 라우트
 app.get("/", (req, res) => {

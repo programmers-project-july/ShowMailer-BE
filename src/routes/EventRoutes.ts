be5c6bfe576e5fe8764e1a1b0995dfdf2928f52b
jieunlee99 <lll3346@naver.com>
2024-07-22 12:24:51 +0900

    fix: event router

--- src/routes/EventRoutes.ts
+++ src/routes/EventRoutes.ts
@@ -2,10 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
-const {
-  getCulturalEvents,
-  getCulturalEventDetail,
-} = require("../controllers/EventController");
+const { getCulturalEvents } = require("../controllers/EventController");
 
 router.get("/", getCulturalEvents); // 목록 조회, 검색
 

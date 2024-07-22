791c1ae33b01a00695d1a678aa7127d748edb187
jieunlee99 <lll3346@naver.com>
2024-07-22 16:45:53 +0900

    fix: swagger error

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
+
 // swagger setting
 const swaggerUi = require("swagger-ui-express");
-const swaggerFile = require("./swagger/swagger-output.json");
+// const swaggerFile = require("./swagger/swagger-output.json");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,7 +14,18 @@ app.use(express.json());
 const eventRouter = require("./routes/EventRoutes");
 const likeRouter = require("./routes/LikeRoutes");
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+const options = {
+  customCssUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui.css",
+};
+const spec = JSON.parse(
+  // fs.readFileSync(path.join(__dirname, "../petstore-api.json"), "utf8")
+  fs.readFileSync(path.join(__dirname, "./swagger/swagger-output.json"), "utf8")
+);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(spec, options));
+
 app.use("/events", eventRouter);
 app.use("/likes", likeRouter);
 

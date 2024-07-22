7243f2c9dfddb7f6e5ea0fe9434fbe0044227ceb
jieunlee99 <lll3346@naver.com>
2024-07-22 11:02:59 +0900

    fix: event detail

--- src/controllers/EventController.ts
+++ src/controllers/EventController.ts
@@ -12,7 +12,7 @@ export const getCulturalEvents = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  const { codename, title } = req.query;
+  const { codename, title, date } = req.query;
   const page = parseInt(req.query.page as string, BATCH_SIZE) || 1; // 페이지 번호, 기본값 1
   const startIndex = (page - 1) * BATCH_SIZE + 1;
   const endIndex = page * BATCH_SIZE;
@@ -20,7 +20,7 @@ export const getCulturalEvents = async (
   try {
     const eventsUrl = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/culturalEventInfo/${startIndex}/${endIndex}/${
       codename || " "
-    }/${title || " "}`;
+    }/${title || " "}/${date || ""}`;
     const response = await axios.get(eventsUrl);
 
     if (response.data.culturalEventInfo.RESULT.CODE === "INFO-000") {
@@ -38,35 +38,5 @@ export const getCulturalEvents = async (
     }
   } catch (error) {
     res.status(500).send("Error retrieving cultural events");
-  }
-};
-
-// 공연 상세 조회
-// 예시) http://localhost:3000/events/국악/서울시국악관현악단 제362회 정기연주회/2024-11-29~2024-11-29
-export const getCulturalEventDetail = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  const { codename, title, date } = req.params;
-  try {
-    const detailUrl = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/culturalEventInfo/1/1/${codename}/${title}/${date}`;
-    const response = await axios.get(detailUrl);
-
-    if (response.data.culturalEventInfo.RESULT.CODE === "INFO-000") {
-      const event = response.data.culturalEventInfo.row.find(
-        (e: any) =>
-          e.CODENAME === codename && e.TITLE === title && e.DATE === date
-      );
-
-      if (event) {
-        res.status(200).json(event);
-      } else {
-        res.status(404).send("Event not found");
-      }
-    } else {
-      res.status(404).send("Event not found");
-    }
-  } catch (error) {
-    res.status(500).send("Error retrieving event detail");
   }
 };
